30c64724b67e19665b28827ce85c9f2acbac7365
Siddardha <158450522+Siddardha75@users.noreply.github.com>
2026-09-14 01:03:05 +0530

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,1 +1,13 @@
 
+
+import requests
+
+def fetch_from_server(url):
+    response = requests.get(url)
+    return {
+      
+        "body" : response.content,
+        "status_code" : response.status_code,
+        "headers" : dict(response.headers)
+
+    }
